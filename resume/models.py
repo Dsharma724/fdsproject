f7446bab18491c74be41b9c650210ec0d89bf25e
Dsharma724 <dhairaysharma7@gmail.com>
2025-09-19 11:34:28 +0530

    Add files via upload

---
+++ resume/models.py
@@ -0,0 +1,14 @@
+
+from django.db import models
+
+class Resume(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True)
+    summary = models.TextField(blank=True)
+    skills = models.TextField(blank=True)
+    experience = models.TextField(blank=True)
+    education = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
